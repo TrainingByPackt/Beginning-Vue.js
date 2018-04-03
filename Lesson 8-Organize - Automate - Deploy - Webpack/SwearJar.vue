6ddc348fa9e4a762c76a40a4e9b7aab0837efbb6
darrenpackt <34567615+darrenpackt@users.noreply.github.com>
2018-04-03 17:19:33 +0530

    Create SwearJar.vue

---
+++ Lesson 8-Organize - Automate - Deploy - Webpack/SwearJar.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+Swears: {{counter}} $$
+<button @click="counter++">+</button>
+</div>
+</template>
+<script>
+export default {
+name: 'swear-jar',
+data () {
+return { counter: 0 }
+}
+}
+</script>
