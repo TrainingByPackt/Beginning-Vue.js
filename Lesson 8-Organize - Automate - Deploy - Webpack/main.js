bc0d8c9be72e7670f314f7bece8ed4f11b74f902
darrenpackt <34567615+darrenpackt@users.noreply.github.com>
2018-04-03 17:16:30 +0530

    Create main.js

---
+++ Lesson 8-Organize - Automate - Deploy - Webpack/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Shaker from './Shaker'
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+el: '#app',
+router,
+template: `
+<div>
+This is a <Shaker>test</Shaker>
+</div>
+`,
+components: { Shaker }
+
+})
