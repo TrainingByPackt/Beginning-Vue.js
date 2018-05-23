ee8e280b6361e3d9c243dea29caba017cbfbc5b0
darrenpackt <34567615+darrenpackt@users.noreply.github.com>
2018-05-23 18:24:44 +0530

    Update randomIntWithin.js

--- Lesson 8-Organize - Automate - Deploy - Webpack/randomIntWithin.js
+++ Lesson 8-Organize - Automate - Deploy - Webpack/randomIntWithin.js
@@ -6,27 +6,3 @@ return Math.floor(Math.random() * max) + min
 }
 
 
-<template>
-<div id="app">
-<h1>Magic dice</h1>
-<button @click="throwDice">Throw!</button>
-{{number}}
-</div>
-</template>
-
-<script>
-import randomIntWithin from './randomIntWithin'
-export default {
-name: 'app',
-data () {
-return {
-number: undefined
-}
-},
-methods: {
-throwDice () {
-this.number = randomIntWithin(1, 6)
-}
-}
-}
-</script>
